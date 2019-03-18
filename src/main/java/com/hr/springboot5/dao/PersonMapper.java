2aa4fca63d5ae3f6bd93f5c2db6066205ea944d4
XZ0612 <2854856406@qq.com>
2019-03-18 11:37:21 +0800

    第一次提交

---
+++ src/main/java/com/hr/springboot5/dao/PersonMapper.java
@@ -0,0 +1,16 @@
+package com.hr.springboot5.dao;
+
+import com.hr.springboot5.entity.Person;
+import java.util.List;
+
+public interface PersonMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Person record);
+
+    Person selectByPrimaryKey(Integer id);
+
+    List<Person> selectAll();
+
+    int updateByPrimaryKey(Person record);
+}
